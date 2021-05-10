a3f4f8df103e2ebfa61165487ee31c297c7eacf8
Ben Silverman <BLSilverman@mgh.harvard.edu>
2021-05-10 17:18:11 -0400

    🐛 Fix incorrect env var name

--- src/lib/session.js
+++ src/lib/session.js
@@ -2,7 +2,7 @@ import { withIronSession } from "next-iron-session";
 
 export default function withSession(handler) {
   return withIronSession(handler, {
-    password: process.env.SECRET_COOKIE_PASSWORD,
+    password: process.env.COOKIE_SECRET,
     cookieName: "subject-id-gen",
     cookieOptions: {
       secure: process.env.NODE_ENV === "production",
