4089c812b601583802d95a15c9c04e9e0e174ff1
Ben Silverman <BLSilverman@mgh.harvard.edu>
2021-05-12 11:36:28 -0400

    Add NProgress progress indicator

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,13 @@
 import { SWRConfig } from 'swr';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import fetchJson from '../lib/fetchJson';
+import 'nprogress/nprogress.css';
 import '../styles/globals.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function SubjectIdGen({ Component, pageProps }) {
   return (
