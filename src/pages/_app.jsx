686ca3c39ca2667111a17ba497c7d0ea85c6e05c
Ben Silverman <BLSilverman@mgh.harvard.edu>
2021-05-20 17:28:30 -0400

    Handle SWR errors on frontend
    Rare occurrence, seems only to be session errors

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,24 +1,36 @@
 import { SWRConfig } from 'swr';
+import { useState } from 'react';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import fetchJson from '../lib/fetchJson';
 import 'nprogress/nprogress.css';
 import '../styles/globals.css';
+import formStyles from '../styles/Form.module.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function SubjectIdGen({ Component, pageProps }) {
+  const [globalError, setGlobalError] = useState('');
+
   return (
     <SWRConfig
       value={{
         fetcher: fetchJson,
         onError: err => {
-          console.error(err);
+          setGlobalError(err.message);
         },
       }}
     >
+      {globalError !== '' && (
+        <div
+          className={formStyles.error}
+          style={{ textAlign: 'center', marginTop: '20px' }}
+        >
+          {globalError}
+        </div>
+      )}
       <Component {...pageProps} />
     </SWRConfig>
   );
