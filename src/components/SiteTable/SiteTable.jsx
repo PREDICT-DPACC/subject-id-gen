b4e139eb7c40263ca5b7f5b58a940cb11fa33a24
Ben Silverman <BLSilverman@mgh.harvard.edu>
2021-05-20 10:39:24 -0400

    SiteTable organize by network

--- src/components/SiteTable/SiteTable.jsx
+++ src/components/SiteTable/SiteTable.jsx
@@ -8,6 +8,46 @@ const SiteTable = ({ sites, mode }) => {
     e.preventDefault();
     router.push(`/sites/${e.target.name}`);
   };
+  const RowsForSite = ({ filteredSites }) =>
+    filteredSites
+      .sort((a, b) => {
+        if (a.siteId < b.siteId) return -1;
+        if (a.siteId > b.siteId) return 1;
+        return 0;
+      })
+      .map(site => (
+        <tr key={site._id}>
+          <td>{site.siteId}</td>
+          <td>{site.name}</td>
+          <td>{site.idseq - 1}</td>
+          <td>{site.members.length}</td>
+          {mode === 'admin' && (
+            <td>
+              {site.members
+                .filter(member => member.siteRole === 'manager')
+                .map((member, index) => (
+                  <>
+                    {member.firstName} {member.lastName}
+                    {index + 1 !==
+                      site.members.filter(mbr => mbr.siteRole === 'manager')
+                        .length && (
+                      <>
+                        , <br />
+                      </>
+                    )}
+                  </>
+                ))}
+              {site.members.filter(member => member.siteRole === 'manager')
+                .length === 0 && ''}
+            </td>
+          )}
+          <td className={styles.actions}>
+            <button type="button" name={site.siteId} onClick={handleManage}>
+              Manage
+            </button>
+          </td>
+        </tr>
+      ));
   return (
     <table className={`${tableStyles.table} ${styles.admintable}`}>
       <tr>
@@ -18,45 +58,22 @@ const SiteTable = ({ sites, mode }) => {
         {mode === 'admin' && <th style={{ width: '15%' }}>Manager(s)</th>}
         <th>Actions</th>
       </tr>
-      {sites
-        .sort((a, b) => {
-          if (a.siteId < b.siteId) return -1;
-          if (a.siteId > b.siteId) return 1;
-          return 0;
-        })
-        .map(site => (
-          <tr key={site._id}>
-            <td>{site.siteId}</td>
-            <td>{site.name}</td>
-            <td>{site.idseq - 1}</td>
-            <td>{site.members.length}</td>
-            {mode === 'admin' && (
-              <td>
-                {site.members
-                  .filter(member => member.siteRole === 'manager')
-                  .map((member, index) => (
-                    <>
-                      {member.firstName} {member.lastName}
-                      {index + 1 !==
-                        site.members.filter(mbr => mbr.siteRole === 'manager')
-                          .length && (
-                        <>
-                          , <br />
-                        </>
-                      )}
-                    </>
-                  ))}
-                {site.members.filter(member => member.siteRole === 'manager')
-                  .length === 0 && ''}
-              </td>
-            )}
-            <td className={styles.actions}>
-              <button type="button" name={site.siteId} onClick={handleManage}>
-                Manage
-              </button>
-            </td>
-          </tr>
-        ))}
+      <tr>
+        <td colSpan={6}>
+          <strong>PRESCIENT</strong>
+        </td>
+      </tr>
+      <RowsForSite
+        filteredSites={sites.filter(site => site.network === 'PRESCIENT')}
+      />
+      <tr>
+        <td colSpan={6}>
+          <strong>ProNET</strong>
+        </td>
+      </tr>
+      <RowsForSite
+        filteredSites={sites.filter(site => site.network === 'ProNET')}
+      />
     </table>
   );
 };
